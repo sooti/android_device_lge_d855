5575fa9dba16a109678b447bfb16828fd09cb752
sooti <sooti>
2015-01-25 12:18:01 +0100

    update dependencies for fusion

--- fusionsp.mk
+++ fusionsp.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/fusion/config/common_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := fusionsp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
