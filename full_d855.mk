50d3cb02b0dd81a86d749e1ae6e47db6675b747b
Rick <samson.rick@gmail.com>
2015-08-24 22:55:46 +0200

    omni changes

--- full_d855.mk
+++ full_d855.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from d855 device
-$(call inherit-product, device/lge/d855/device.mk)
+$(call inherit-product, device/lge/d855/d855.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d855
